c3f3d11c6dca29553fd2721e46d2bc60c6096ae7
AmbujaAK <itsambuja@gmail.com>
2020-11-24 22:01:31 +0530

    starter file added

---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
