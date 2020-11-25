b58446b34e0a1119b7801c569ea2e04d53f05194
AmbujaAK <itsambuja@gmail.com>
2020-11-25 08:12:29 +0530

    tabs created

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter_whatsapp_clone/home_page.dart';
+
+List<CameraDescription> cameras;
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  cameras = await availableCameras();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      title: 'WhatsApp',
+      theme: ThemeData(
+        primaryColor: Color(0xff075E54),
+        accentColor: Color(0xff25D366),
+      ),
+      debugShowCheckedModeBanner: false,
+      home: HomePage(cameras: cameras),
+    );
   }
 }
